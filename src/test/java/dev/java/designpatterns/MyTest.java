307785ca1f29ca5fb3f6594a714628633a29dd61
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-09-18 12:44:32 +0530

    Maven - JUNIT 5 Setup

---
+++ src/test/java/dev/java/designpatterns/MyTest.java
@@ -0,0 +1,11 @@
+package dev.java.designpatterns;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    void setupOk() {
+        System.out.println("JUnit Setup Successful");
+    }
+}
